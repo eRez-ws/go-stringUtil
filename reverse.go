45262a19ec9863e62abdb42d8e761b7253d74fc0
ErezHuberman <erez.huberman@whitesourcesoftware.com>
2022-02-06 13:54:47 +0200

    added fmt

--- reverse.go
+++ reverse.go
@@ -1,13 +1,14 @@
 // Package stringutil contains utility functions for working with strings.
 package stringUtil
 
-
+import "fmt"
 
 // Reverse returns its argument string reversed rune-wise left to right.
-func Reverse(s string) string {	
+func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
+	fmt.Println(string(r))
 	return string(r)
 }
